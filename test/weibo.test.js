71d1a4d1c0eb15c129e5645c48f408f501ddc3ac
徐楷 <15112707574@qq.com>
2020-06-04 14:59:40 +0800

    test: update test

--- test/weibo.test.js
+++ test/weibo.test.js
@@ -2,7 +2,7 @@
  * @Author: xukai
  * @Date: 2020-06-01 18:10:58
  * @Last Modified by: xukai
- * @Last Modified time: 2020-06-03 15:16:28
+ * @Last Modified time: 2020-06-04 14:56:37
  */
 const assert = require('assert')
 const supertest = require('supertest')
@@ -13,6 +13,7 @@ const userPrefix = '/api/user'
 const blogPrefix = '/api/blog'
 const profilePrefix = '/api/profile'
 const squarePrefix = '/api/square'
+const atMePrefix = '/api/atMe'
 const userName = `u_${Date.now()}`
 const password = `p_${Date.now()}`
 let cookie = ''
@@ -75,6 +76,24 @@ describe('user', () => {
 
   it('squareLoadMore', async () => {
     const res = await request.get(squarePrefix + '/loadMore/0')
+      .set('cookie', cookie)
+    assert.strictEqual(res.body.errno, 0)
+  })
+
+  it('follow', async () => {
+    const res = await request.post(profilePrefix + '/follow').send({ userId: 1 })
+      .set('cookie', cookie)
+    assert.strictEqual(res.body.errno, 0)
+  })
+
+  it('unFollow', async () => {
+    const res = await request.post(profilePrefix + '/unFollow').send({ userId: 1 })
+      .set('cookie', cookie)
+    assert.strictEqual(res.body.errno, 0)
+  })
+
+  it('atMe', async () => {
+    const res = await request.get(atMePrefix + '/loadMore/0').send({ pageIndex: 0 })
       .set('cookie', cookie)
     assert.strictEqual(res.body.errno, 0)
   })
