a30e2c21b49e5acb29b0b171d14f2ce27ce26deb
徐楷 <15112707574@qq.com>
2020-06-04 15:07:56 +0800

    ci(pm2): update config

---
+++ config/production.js
@@ -0,0 +1,16 @@
+'use strict'
+
+module.exports = {
+  mysql: {
+    host: 'localhost',
+    db: 'weibo',
+    user: 'root',
+    pass: '123456',
+    port: 3306,
+    dbCache: false
+  },
+  REDIS_CONF: {
+    host: 'localhost',
+    port: 6379
+  }
+}
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -19,10 +19,10 @@ module.exports = {
 
   deploy: {
     production: {
-      user: 'node',
-      host: '212.83.163.1',
+      user: 'root',
+      host: '120.27.244.150',
       ref: 'origin/master',
-      repo: 'git@github.com:repo.git',
+      repo: 'git@github.com:xukaike/my-weibo.git',
       path: '/var/www/production',
       'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production'
     }
